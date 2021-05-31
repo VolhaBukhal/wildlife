1089a22b767a8246de090cebc3e77add4b3bd755
olga <o.bukhal@gmail.com>
2021-05-31 07:37:15 +0300

    sart of slider section

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+//slick slider initializing  https://kenwheeler.github.io/slick/
+  $('.news-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 1200,
+    slidesToShow: 3
+    // autoplay: true,
+    // autoplaySpeed: 4000
+  });
+
